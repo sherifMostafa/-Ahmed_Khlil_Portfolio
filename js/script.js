513bfeab8b722d9ea93ae49a87f47b00737162bc
unknown <sherifmostafa628@gmail.com>
2021-03-13 23:23:32 +0200

    My Portfolio

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// onscroll behavior
+AOS.init();
+const navbar = document.querySelector('.navbar');
+window.onscroll = function() {
+  
+  if(this.scrollY > 50)
+    navbar.classList.add('bg-light')
+  else if(!navbar.classList.contains('open')){
+    navbar.classList.remove('bg-light')
+  } 
+}
+document.querySelector('.navbar-toggler').onclick = function() {
+  if(!navbar.classList.contains('bg-light')) {
+          navbar.classList.add('bg-light');
+          navbar.classList.add('open');
+  }
+  else {
+        navbar.classList.remove('bg-light');
+        navbar.classList.remove('open');
+  }
+}
+
+
